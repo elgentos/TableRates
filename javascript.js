8ebb2962a6a6a6f1b90318265288d9ae076621be
Peter Jaap Blaakmeer <peterjaap@elgentos.nl>
2015-02-26 14:25:52 +0100

    Fix javascript.js - combined two files with resulted in errors

--- javascript.js
+++ javascript.js
@@ -1,12 +1,58 @@
 function checkEU() {
     if(document.location.href.indexOf("twoLetterCodes")>0) {
-        var EU = new Array("BE","BG","CY","DK","DE","EE","FI","FR","GR","HU","IR","IT","LV","LT","LU","MT","NL","AT","PL","PT","RO","SK","SI","ES","CZ","GB","SE");
+        var EU = new Array("BE","BG","CY","DK","DE","EE","FI","FR","GR","HU","HR","IE","IT","LV","LT","LU","MT","NL","AT","PL","PT","RO","SK","SI","ES","CZ","GB","SE");
     } else {
-        var EU = new Array("BEL","BGR","CYP","DNK","DEU","EST","FIN","FRA","GRC","HUN","IRN","ITA","LVA","LTU","LUX","MLT","NLD","AUT","POL","PRT","ROU","SVK","SVN","ESP","CZE","GBR","SWE");
+        var EU = new Array("BEL","BGR","CYP","DNK","DEU","EST","FIN","FRA","GRC","HUN","HRV","IRL","ITA","LVA","LTU","LUX","MLT","NLD","AUT","POL","PRT","ROU","SVK","SVN","ESP","CZE","GBR","SWE");
     }
     for(i=0;i<EU.length;i++) {
         $("input[value='"+EU[i]+"']").attr('checked','checked');
     }
+}
+
+function clearValues() {
+    $("input:checked").each(function (index,element) {
+        e = $(element).val();
+        $("input[name^='price_" + e + "']").val('');
+        $("input[name^='from_" + e + "']").val('');
+    });
+}
+
+function checkOutsideEU() {
+    if(document.location.href.indexOf("twoLetterCodes")>0) {
+        var EU = new Array("BE","BG","CY","DK","DE","EE","FI","FR","GR","HU","HR","IR","IT","LV","LT","LU","MT","NL","AT","PL","PT","RO","SK","SI","ES","CZ","GB","SE");
+    } else {
+        var EU = new Array("BEL","BGR","CYP","DNK","DEU","EST","FIN","FRA","GRC","HUN","HRV","IRN","ITA","LVA","LTU","LUX","MLT","NLD","AUT","POL","PRT","ROU","SVK","SVN","ESP","CZE","GBR","SWE");
+    }
+    /*for(i=0;i<EU.length;i++) {
+        $("input[value='"+EU[i]+"']").attr('checked','checked');
+    }*/
+    $("input[name^='country_']").each(function (index,element) {
+        if(EU.indexOf($(element).val()) === -1) {
+            $(element).attr('checked','checked');
+        }
+    });
+}
+
+function checkOutsideEuro() {
+    if(document.location.href.indexOf("twoLetterCodes")>0) {
+        var EU = new Array("AL","AD","AM","AT","AZ","BY","BE","BA","BG","HR","CY","CZ","DK","EE","FI","FR","GE","DE","GR","HU","IS","IE","IT","KZ","LV","LI","LT","LU","MK","MT","MD","MC","ME","NL","NO","PL","PT","RO","RU","SM","RS","SK","SI","ES","SE","CH","TR","UA","GB","VA");
+    } else {
+        var EU = new Array("ALB","AND","ARM","AUT","AZE","BLR","BEL","BIH","BGR","HRV","CYP","CZE","DNK","EST","FIN","FRA","GEO","DEU","GRC","HUN","ISL","IRL","ITA","KAZ","LVA","LIE","LTU","LUX","MKD","MLT","MDA","MCO","MNE","NLD","NOR","POL","PRT","ROU","RUS","SMR","SRB","SVK","SVN","ESP","SWE","CHE","TUR","UKR","GBR","VAT");
+    }
+    /*for(i=0;i<EU.length;i++) {
+        $("input[value='"+EU[i]+"']").attr('checked','checked');
+    }*/
+    $("input[name^='country_']").each(function (index,element) {
+        if(EU.indexOf($(element).val()) === -1) {
+            $(element).attr('checked','checked');
+        }
+    });
+}
+
+function reset() {
+    $("input[name^='country_']").each(function (index,element) {
+        $(element).removeAttr('checked');
+    });
 }
 
 function checkEuro() {
@@ -47,6 +93,13 @@ $(window).ready(function () {
         }
     });
 });
+
+function addRowSelectedCountries() {
+    $("input:checked").each(function (index,element) {
+        li = $(element).closest('li');
+        li.find('a.addRow').click();
+    });
+}
 
 function changeLabel() {
     var condition = $("#carriers_tablerate_condition_name").val();
